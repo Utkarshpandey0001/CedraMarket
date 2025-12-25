114a5cc30ebb3a50d63a9bd3a908ce19940922e5
Utkarshpandey0001 <rajutkarshpandey2003@gmail.com>
2025-12-25 19:56:31 +0530

    fix the type error in module address

--- frontend/app/constant.ts
+++ frontend/app/constant.ts
@@ -1,4 +1,16 @@
-export const MODULE_ADDRESS = process.env.NEXT_PUBLIC_MODULE_ADDRESS;
+// export const MODULE_ADDRESS = process.env.NEXT_PUBLIC_MODULE_ADDRESS;
 
+
+// export const GENERIC_TYPE = `${MODULE_ADDRESS}::market_v6::MarketItem`;
+
+
+
+
+
+
+
+export const MODULE_ADDRESS = 
+  process.env.NEXT_PUBLIC_MODULE_ADDRESS || 
+  "0x2af00cec9331ad1402032cf0612b904ed51eb2d7c401e38011c7e6b08cffc8f8"; 
 
 export const GENERIC_TYPE = `${MODULE_ADDRESS}::market_v6::MarketItem`;
